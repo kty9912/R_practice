d0ab9fb3f332700ec263e2dba702853029cc3f63
kty9912 <kty9912@naver.com>
2023-01-13 11:27:20 +0900

    1장

--- script1.R
+++ script1.R
@@ -71,3 +71,31 @@ qplot(x)
 
 # data에 mpg, x축에 hwy 변수 지정해 그래프 생성
 qplot(data=mpg, x=hwy)
+
+# 파라미터 수정
+# x축 cty
+qplot(data=mpg, x = cty)
+
+# x축 drv, y축 hwy
+qplot(data=mpg, x=drv, y=hwy)
+
+# x축 drv, y축 hwy, 선 그래프 형태
+qplot(data=mpg, x=drv, y=hwy, geom = "line")
+
+# x축 drv, y축 hwy, 상자 그림 형태
+qplot(data=mpg, x=drv, y=hwy, geom = "boxplot")
+
+# x축 drv, y축 hwy, 상자 그림 형태, drv별 색 표현
+qplot(data=mpg, x=drv, y=hwy, geom = "boxplot", colour = drv)
+
+# qplot 함수 메뉴얼 출력
+# 함수가 들어있는 패키지를 로드한 상태에서만 메뉴얼 출력코드 실행함
+?qplot
+
+
+# 혼자서 해보기
+score <- c(80,60,70,50,90)
+score
+mean(score)
+mean <- mean(score)
+mean
