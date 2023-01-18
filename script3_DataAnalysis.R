821801cdab13741aba6c15bafadb548069e376d9
kty9912 <kty9912@naver.com>
2023-01-18 17:08:20 +0900

    05

--- script3_DataAnalysis.R
+++ script3_DataAnalysis.R
@@ -44,3 +44,78 @@ str(mpg)   # 데이터 속성 확인
 
 # 요약 통계량 출력
 summary(mpg)
+
+
+# 05-2 변수명 바꾸기
+# dplyr 패키지의 rename()을 이용해 변수명 바꾸기
+df_raw <- data.frame(var1 = c(1,2,1),
+                     var2 = c(2,3,2))
+df_raw
+
+# dplyr 패키지 로드
+library(dplyr)
+
+# 데이터프레임 복사본 만들기
+df_new <- df_raw
+df_new
+
+# 변수명 바꾸기
+df_new <- rename(df_new, v2 = var2) # var2를 v2로 수정
+df_new
+# 원본과 비교
+df_raw
+df_new
+
+# 혼자서 해보기
+mpg <- as.data.frame(ggplot2::mpg)
+mpg
+mpg_new <- mpg
+mpg_new
+mpg_new <- rename(mpg_new, city=cty, highway=hwy)
+head(mpg_new)
+
+
+# 파생변수 만들기 (평균)
+df <- data.frame(var1 = c(4,3,8),
+                 var2 = c(2,6,1))
+df
+
+# var_sum 파생변수 생성
+df$var_sum <- df$var1 + df$var2  
+df
+
+# var_mean 파생변수 생성
+df$var_mean <- (df$var1 + df$var2)/2  
+df
+
+# 통합 연비 변수 생성
+mpg$total <- (mpg$cty + mpg$hwy)/2
+head(mpg)
+
+# 통합 연비 변수 평균
+mean(mpg$total)
+
+# 조건문 활용 파생변수
+# 요약 통계량 산출
+summary(mpg$total) 
+
+# 히스토그램 생성
+hist(mpg$total)
+
+# 조건문 함수
+ifelse(mpg$total >= 20, "pass", "fail")
+# (조건, 조건이 맞을때, 조건이 맞지 않을때)
+
+# 20이상이면 pass, 그렇지 않으면 fail
+mpg$test <- ifelse(mpg$total >=20, "pass", "fail")
+
+head(mpg, 20)  # 데이터 확인
+
+# 연비 합격 빈도표 생성
+table(mpg$test)
+
+# ggplot2 로드
+library(ggplot2)
+
+# 연비 합격 빈도 막대 그래프 생성
+qplot(mpg$test)
